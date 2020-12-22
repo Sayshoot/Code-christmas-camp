72c3c56ba515c85ed0abc15dfd0807a18b90e60e
Sayshoot <76474210+Sayshoot@users.noreply.github.com>
2020-12-22 11:12:06 +0100

    initial push
    initial push

---
+++ main.js
@@ -0,0 +1,33 @@
+$("#top").text("Hello world");
+
+function Main() {
+
+}
+
+function LaunchGame(id, tiles) {
+
+}
+
+function InitLine(id, tile) {
+
+}
+
+function Rng(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function InitTab() {
+
+}
+
+function ChangeColor(id, nb) {
+
+}
+
+function ClearColor(id) {
+
+}
+
+function GameLogic(event) {
+
+}
